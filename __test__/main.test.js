98f28657ab01d01d53833b9d8edc906022326870
Agustín More <a.more@fi.uba.ar>
2022-04-10 14:55:39 -0300

    :wrench: Add test support

---
+++ __test__/main.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import { App } from '../App';
+
+test('basic test', () => {
+  const { getByText } = render(<App/>);
+
+  const mainText = getByText(/Open up/);
+  expect(mainText).not.toBeEmpty();
+});
