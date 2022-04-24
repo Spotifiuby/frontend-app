ee141eb1632b730ce55cb2e41afad720a342511d
Agustín More <a.more@fi.uba.ar>
2022-04-24 17:48:15 -0300

    :bug: fix Android compatibility issues

---
+++ src/Components/ErrorCard.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Text, StyleSheet } from 'react-native';
+import { errorContainer, paddedContainer, tenMarginTop } from '../theme';
+
+const ErrorCard = ({ errorMessage }) => {
+  return (
+    errorMessage
+      ? <Text style={errorStyles.card}>{errorMessage}</Text> : null
+  );
+};
+
+const errorStyles = StyleSheet.create({
+  card: {
+    ...errorContainer,
+    ...paddedContainer,
+    ...tenMarginTop,
+    borderRadius: 3,
+  },
+});
+
+ErrorCard.propTypes = {
+  errorMessage: PropTypes.string.isRequired,
+};
+
+export default ErrorCard;
