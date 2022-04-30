2c41aadd91d95d9de9e5c99b7ea7fd8aed673e0a
bauti <jbrodriguez@fi.uba.ar>
2022-04-30 01:09:28 -0300

    login draft

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
-import AppContainer from './src/Components/AppContainer';
-import i18next from './src/i18n/i18n';
-import RNLanguageDetector from './src/i18n/RNLanguageDetector';
-import { oneUnitFlex, textColor } from './src/theme';
+// import { SafeAreaView, StyleSheet } from 'react-native';
+// import AppContainer from './src/Components/AppContainer';
+// import i18next from './src/i18n/i18n';
+// import RNLanguageDetector from './src/i18n/RNLanguageDetector';
+// import { oneUnitFlex, textColor } from './src/theme';
 
-const App = () => {
-  i18next.use(RNLanguageDetector);
+// const App = () => {
+//   i18next.use(RNLanguageDetector);
+//   return (
+//     <SafeAreaView style={styles.main}>
+//       <AppContainer />
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   main: {
+//     ...oneUnitFlex,
+//     ...textColor,
+//   },
+// });
+
+// export default App;
+// import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./src/Components/Login";
+import Register from "./src/Components/Register";
+import Reset from "./src/Components/Reset";
+import Dashboard from "./src/Components/Dashboard";
+function App() {
   return (
-    <SafeAreaView style={styles.main}>
-      <AppContainer />
-    </SafeAreaView>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
   );
-};
-
-const styles = StyleSheet.create({
-  main: {
-    ...oneUnitFlex,
-    ...textColor,
-  },
-});
-
+}
 export default App;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,34 +1,89 @@
-import { StyleSheet, Image } from 'react-native';
-import PropTypes from 'prop-types';
-import { useTranslation } from 'react-i18next';
-import LoginForm from './LoginForm';
-import logo from '../../assets/logo.png';
+// import { StyleSheet, Image } from 'react-native';
+// import PropTypes from 'prop-types';
+// import { useTranslation } from 'react-i18next';
+// import LoginForm from './LoginForm';
+// import logo from '../../assets/logo.png';
 
-const Login = ({ setToken }) => {
-  const { t } = useTranslation();
+// const Login = ({ setToken }) => {
+//   const { t } = useTranslation();
 
+//   return (
+//     <>
+//       <Image
+//         style={styles.logo}
+//         source={logo}
+//         accessibilityLabel={t('Spotifiuby logo image')}
+//       />
+//       <LoginForm setToken={setToken} />
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   logo: {
+//     height: 200,
+//     marginTop: '25%',
+//     width: 200,
+//   },
+// });
+
+// Login.propTypes = {
+//   setToken: PropTypes.func.isRequired,
+// };
+
+// export default Login;
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) navigate("/dashboard");
+  }, [user, loading]);
   return (
-    <>
-      <Image
-        style={styles.logo}
-        source={logo}
-        accessibilityLabel={t('Spotifiuby logo image')}
-      />
-      <LoginForm setToken={setToken} />
-    </>
+    <div className="login">
+      <div className="login__container">
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="login__btn"
+          onClick={() => signInWithEmailAndPassword(email, password)}
+        >
+          Login
+        </button>
+        <button className="login__btn login__google" onClick={signInWithGoogle}>
+          Login with Google
+        </button>
+        <div>
+          <Link to="/reset">Forgot Password</Link>
+        </div>
+        <div>
+          Don't have an account? <Link to="/register">Register</Link> now.
+        </div>
+      </div>
+    </div>
   );
-};
-
-const styles = StyleSheet.create({
-  logo: {
-    height: 200,
-    marginTop: '25%',
-    width: 200,
-  },
-});
-
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired,
-};
-
+}
 export default Login;
